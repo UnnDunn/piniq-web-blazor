781034f4944253ab511102341c3e14a25833b558
Uchendu Nwachuku <uchendu@unndunn.com>
2025-02-04 16:12:55 -0500

    Remove hardcoded OAuth token from PinballCatalogService
    The hardcoded OAuth token has been replaced with an empty string to improve security and prevent sensitive data exposure in the source code. This change ensures better compliance with secure coding practices.

--- PinIQ.Blazor/Services/PinballCatalogService.cs
+++ PinIQ.Blazor/Services/PinballCatalogService.cs
@@ -7,7 +7,7 @@ namespace PinIQ.Blazor.Services;
 
 public class PinballCatalogService
 {
-    private const string OauthToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJvZ2lkIjoiVGVzdElEIiwib2dpc3MiOiJTZWxmIiwiYXVkIjpbImh0dHBzOi8vbG9jYWxob3N0OjUwMDEiLCJodHRwOi8vbG9jYWxob3N0OjUwMDAiLCJodHRwOi8vbG9jYWxob3N0OjgwMDAiLCJodHRwczovL2xvY2FsaG9zdDo4MDAxIl0sIm5iZiI6MTczODcwMjU3NiwiZXhwIjoxNzM4NzA0Mzc2LCJpc3MiOiJwaW5pcS1kZXYifQ.4LvTo2a71VqS1VtvDG6KQ5LQN8K9LvHB5gCGc3smB0I";
+    private const string OauthToken = "";
     private Dictionary<int, CatalogSnapshot> _catalogSnapshotCache = new();
     private readonly HubConnection _pinballCatalogNotificationHubConnection;
     private readonly HttpClient _httpClient;
